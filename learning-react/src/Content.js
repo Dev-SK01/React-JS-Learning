e51960b4b7ebc1c0a20622841d1ecea978a7e26f
Dev-SK01 <srik3713@gmail.com>
2023-12-25 12:47:37 +0530

    List & keys , props & Drilling completed

--- learning-react/src/Content.js
+++ learning-react/src/Content.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useState } from 'react';
 import './App.css'
+import { Footer } from './Footer';
 
-
-export const Content = () => {
+// ---------  Examples of UseState --------
+const Usestate = () => {
 
     function textChange() {
         const textList = ['Grow', 'Give'];
@@ -17,14 +18,81 @@ export const Content = () => {
         console.log(event.target);
         alert('Hello React Dev ^_^');
     }
-    //  changing the text 
+    //  changing the text  USESTATE
     const [initText, setText] = useState('Earn');
-    const [person , setPerson] = useState('Me');
+    const [person, setPerson] = useState('Me');
 
     return (
-        <section className='Content'>
+        <>
             <p className='Text'>Let's {initText} Money With {person} !</p> <button onClick={textChange}>Change Text</button>
             <button onClick={(event) => handleClick(event)}>SayHi):</button>
+        </>
+    )
+}
+
+const List_Keys = () => {
+    const [itemobj , setItem] = useState([
+        {
+            id: 1,
+            checked: false,
+            data: 'Test Item'
+        },
+        {
+            id: 2,
+            checked: true,
+            data: 'Test Item'
+        }
+    ])
+       
+    function handlechange(id){
+        const list_item = itemobj.map((item) => (
+            item.id === id ? {...item , checked :!item.checked} : item 
+        )) 
+
+        setItem(list_item);
+    }
+
+    function handleDelete(id){
+        const list_item = itemobj.filter((item) => (
+            item.id != id
+        )) 
+
+        setItem(list_item);
+    }
+    return (
+        <section className='list-div'>
+            <ul>
+                {itemobj.map((item) => (
+                    <li className='list-item' key={item.key}>
+                        <input type="checkbox" name="checkbox" id='checkbox' 
+                        checked={item.checked} 
+                        onChange={() => (handlechange(item.id))} />
+                        <label htmlFor="checkbox" className='item-data'>
+                            {item.data}
+                        </label>
+                        <button className='list-btn' 
+                        onClick={() => (handleDelete(item.id))}> 
+                        Delete</button>
+                    </li>
+                ))}
+            </ul>
         </section>
+    )
+}
+
+// -------------   Main Content Component -------------
+export const Content = () => {
+
+
+    return (
+        <>
+        <section className='Content'>
+            <Usestate />
+            <List_Keys />
+        </section>
+        <>
+         <Footer length={List_Keys.length} react="Items"/>
+        </>
+        </>
     )
 }
--- learning-react/src/Footer.js
+++ learning-react/src/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
 
-const Footer = () => {
+const Footer = ({react,length}) => {
     const year = new Date();
   return (
     <footer className='footer'  style={{fontSize:'1.5rem' , backgroundColor:'blueviolet'}}>
-        <p>CopyRight &copy; {year.getFullYear()}.All Rights Reserved.</p>
+        <p>CopyRight &copy; {year.getFullYear()}.All Rights Reserved. {length} : {react}</p>
     </footer>
   )
 }
 
+Footer.defaultProps = {
+   react : 'React'
+}
 export  {Footer};
