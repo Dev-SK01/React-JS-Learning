3d706b529831dfcd2ec12e8922b8267f1fb60c42
Dev-SK01 <srik3713@gmail.com>
2023-12-30 15:35:38 +0530

    Project01 Finished

--- learning-react/src/Color_show.js
+++ learning-react/src/Color_show.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './color-show.css'
+
+
 function Color_show() {
+
+ const[color , setColor] = useState('white');
+ 
+ const [textColor , setTextColor] = useState('black')
+
+function ControlColor() {
+  (color ==  'white'  ||  color ==  'black') ? (textColor == 'black' ? setTextColor('white'): setTextColor('black')) : setTextColor(textColor);
+}
+
   return (
    <>
-   <p>Sampl  test</p>
+   <section className='color-holder'>
+    <div className="color-div" style={{backgroundColor:color}}>
+      <p style={{color:textColor}}>{color}</p>
+      <span style={{color:textColor}}>#{color}</span>
+    </div>
+    </section>
+    <input 
+      type="text"  
+      id="color" 
+      placeholder='Enter Color'
+      className='color-input'
+      value={color}
+      onChange={(e) => (setColor(e.target.value)) }
+    />
+    <button
+    onClick={ () => ControlColor()}
+    >
+    Toggle Color
+    </button>
    </>
   )
 }
